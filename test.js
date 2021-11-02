dd5d1b633a895a88181c12aed9e976045d3ddd13
BobT0ad <jeremy.bertronski@gmail.com>
2021-11-01 20:29:27 -0500

    Adding files

--- test.js
+++ test.js
@@ -57,6 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
             var div4 = document.getElementById('freq');
             div4.innerHTML += freqString;
         })
+    simplyPepeLaugh();
 })
 
 // Avg Seconds to HH:MM:SS
@@ -65,4 +66,32 @@ function formatChange(avg) {
     date.setSeconds(avg);
     var avgResult = date.toISOString().substr(12, 7);
     return avgResult;
+}
+
+function simplyPepeLaugh() {
+    // Display the countdown timer in an element
+
+    // Set the date we're counting up from
+    var countDownDate = new Date("May 29, 2020 8:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = now - countDownDate;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="wrTimer"
+        document.getElementById("wrTimer").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s " + "since Simply's last WR!";
+
+    }, 1000);
 }
